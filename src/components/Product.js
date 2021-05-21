a6657e5bc3d0be13da42807394ec364b86ed03a9
Amrit <amritghimire741443@gmail.com>
2021-05-21 22:48:41 +0545

    Animation added to product.js file

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from "next/image"
 import {StarIcon} from "@heroicons/react/solid"
 import Currency  from "react-currency-formatter"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
+
+
+
+
+
+
 function Product({title, image, price, category, description}) {
+
+    useEffect(()=>{
+        Aos.init({duration:2000})
+    },[])
     
 
     const [rating] = useState(
@@ -16,7 +28,7 @@ function Product({title, image, price, category, description}) {
 
     
     return (
-        <div className="relative flex flex-col m-5 bg-white z-20 p-10">
+        <div className="  relative flex flex-col m-5 bg-white z-20 p-10 " data-aos="fade-up">
             <p className="absolute top-2 right-2 text-sm italic text-gray-400">{category}</p>
             
             <Image src={image} height={200} width={200} objectFit="contain" />
@@ -31,7 +43,7 @@ function Product({title, image, price, category, description}) {
                 
             </div>
 
-           <p className="text-xs my-2 line-clamp-2" >{description}</p>
+           <p className="text-xs my-2 line-clamp-2" data-aos="fade-out" data-aos-duration="3000" >{description}</p>
            <div className="mb-5">
                <Currency  quantity={price} currency="USD"/>
            </div>
